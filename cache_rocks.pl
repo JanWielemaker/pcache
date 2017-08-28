3e07c05fd7d6d2646a180f2db77791751004fb52
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-08-28 14:37:32 +0200

    ADDED: Support partially filled caches.

--- cache_rocks.pl
+++ cache_rocks.pl
@@ -53,7 +53,8 @@ This module allows for transparent caching of query results.
 :- meta_predicate
     cached(0),
     forget(0),
-    cache_property(0, ?).
+    cache_property(0, ?),
+    offset_check(+, 0, +).
 
 :- dynamic
     rocks_d/2.
@@ -92,14 +93,107 @@ rocks(_) :-
 cached(G) :-
     rocks(DB),
     goal_signature(G, Signature, Vars),
-    (   rocks_get(DB, Signature, cache(G, Answers, _Time, _Hash))
-    ->  member(Vars, Answers)
-    ;   findall(Vars, G, Answers),
-        get_time(Now),
-        functor(Signature, Hash, _),
-        rocks_put(DB, Signature, cache(G, Answers, Now, Hash)),
-        member(Vars, Answers)
+    (   rocks_get(DB, Signature, cache(G, Answers, State, _Time, _Hash))
+    ->  from_db(State, Vars, Answers, restart(G, Signature, DB))
+    ;   cache(G, Signature, Vars, [], DB)
     ).
+
+cache(G, Sign, Vars, Sofar, DB) :-
+    get_time(Now),
+    copy_term(G+Sign, Goal+Signature),
+    functor(Signature, Hash, _),
+    Cache = cache(Goal, _Answers, _State, Now, Hash),
+    (   Sofar == []
+    ->  Enum = (G, add_answer(Set, Vars))
+    ;   Enum = (offset_check(Vars, G, Sofar), add_answer(Set, Vars))
+    ),
+    setup_call_catcher_cleanup(
+        answer_set(Sofar, Set),
+        Enum,
+        Catcher,
+        commit(Catcher, Set, Signature, Cache, DB)).
+
+commit(exit, Set, Signature, Cache, DB) :-
+    answers(Set, Answers),
+    set_cache(Cache, Answers, complete),
+    rocks_put(DB, Signature, Cache).
+commit(fail, Set, Signature, Cache, DB) :-
+    answers(Set, Answers),
+    set_cache(Cache, Answers, complete),
+    rocks_put(DB, Signature, Cache).
+commit(!, Set, Signature, Cache, DB) :-
+    answers(Set, Answers),
+    set_cache(Cache, Answers, partial),
+    rocks_put(DB, Signature, Cache).
+commit(exception(E), Set, Signature, Cache, DB) :-
+    answers(Set, Answers),
+    set_cache(Cache, Answers, exception(E)),
+    rocks_put(DB, Signature, Cache).
+commit(external_exception(_), Set, Signature, Cache, DB) :-
+    answers(Set, Answers),
+    set_cache(Cache, Answers, partial),
+    rocks_put(DB, Signature, Cache).
+
+from_db(complete, Vars, Answers, _Restart) :-
+    member(Vars, Answers).
+from_db(partial, Vars, Answers, restart(G, Signature, DB)) :-
+    (   member(Vars, Answers)
+    ;   cache(G, Signature, Vars, Answers, DB)
+    ).
+from_db(exception(E), Vars, Answers, _Restart) :-
+    (   member(Vars, Answers)
+    ;   throw(E)
+    ).
+
+
+set_cache(cache(_Goal, Answers, State, _Now, _Hash), Answers, State).
+
+%!  answer_set(+List0, -Answers) is det.
+%!  add_answer(+Answers, +Answer) is det.
+%!  answers(+Answers, -List) is det.
+
+answer_set([], answers(List, List)) :-
+    !,
+    List = [$|_].
+answer_set(Set0, answers([$|OpenSet], Tail)) :-
+    open_list(Set0, OpenSet, Tail2),
+    Tail = Tail2.                       % delay unification to avoid
+                                        % Tail becoming a reference chain
+open_list([Last], T, T) :-
+    !,
+    T = [Last|_].
+open_list([H|T0], [H|T], Last) :-
+    open_list(T0, T, Last).
+
+add_answer(Set, A) :-
+    arg(2, Set, T0),
+    duplicate_term(A, A2),
+    nb_linkarg(2, T0, [A2|_]),
+    arg(2, T0, T),
+    nb_linkarg(2, Set, T).
+
+answers(answers([$|Answers], T), Answers) :-
+    arg(2, T, []).
+
+%!  offset_check(+Template, :Goal, +Expected)
+%
+%   Skip the first Expected answers from Goal.   Raise an exception if a
+%   produced answer is not a variant of Template.
+
+offset_check(Template, Goal, Expected) :-
+    State = state(Expected),
+    Goal,
+    arg(1, State, Answers),
+    (   Answers == []
+    ->  true
+    ;   Answers = [First|More],
+        (   First =@= Template
+        ->  nb_linkarg(1, State, More),
+            fail
+        ;   throw(error(consistency_error(Goal, Template, First),_))
+        )
+    ).
+
 
 %!  cache_property(:Goal, ?Property) is nondet.
 %!  cache_properties(:Goal, ?Properties:dict) is nondet.
@@ -132,11 +226,12 @@ property(hash(Hash), cache(_, _, _, Hash)).
 cache_properties(M:SubGoal,
                  cache_properties{count:Count,
                                   time_cached:Time,
+                                  state:State,
                                   hash:Hash
                                  }) :-
     rocks(DB),
     current_module(M),
-    Cache = cache(M:SubGoal, Answers, Time, Hash),
+    Cache = cache(M:SubGoal, Answers, State, Time, Hash),
     rocks_enum(DB, _, Cache),
     length(Answers, Count).
 
