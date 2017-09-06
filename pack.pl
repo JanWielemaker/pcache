f02a6549c7c30d7710ffdb9aec8c9772ffb6a1e8
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-09-06 14:26:30 +0200

    Added pack meta file

---
+++ pack.pl
@@ -0,0 +1,7 @@
+name(pcache).
+version('0.1.0').
+title('Persistent answer cache').
+keywords([cache, memoize, persistency]).
+author( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
+home('https://github.com/JanWielemaker/pcache' ).
+download('https://github.com/JanWielemaker/pcache/releases/*.zip').
